dab34a55adeeeb5f6f3180627fb684590c7ed36d
psg <1999byh@naver.com>
2025-06-04 10:17:28 +0900

    feat: 승객 유효성 검증 추가
    - 승객과 택시 기사의 유효성 검증 기준이 달라 일치시켰습니다.

--- src/main/java/goorm/humandelivery/driver/dto/request/RegisterTaxiRequest.java
+++ src/main/java/goorm/humandelivery/driver/dto/request/RegisterTaxiRequest.java
@@ -3,6 +3,7 @@ package goorm.humandelivery.driver.dto.request;
 import goorm.humandelivery.driver.domain.FuelType;
 import goorm.humandelivery.driver.domain.TaxiType;
 import goorm.humandelivery.shared.annotation.ValidEnum;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -12,11 +13,13 @@ import lombok.*;
 @AllArgsConstructor
 public class RegisterTaxiRequest {
 
+    @NotBlank(message = "차종을 입력해 주세요.")
     private String model;
 
     @ValidEnum(enumClass = TaxiType.class, message = "지원하지 않는 택시 타입입니다.")
     private String taxiType;
 
+    @NotBlank(message = "차량번호를 입력해 주세요.")
     private String plateNumber;
 
     @ValidEnum(enumClass = FuelType.class, message = "지원하지 않는 연료 타입입니다.")
