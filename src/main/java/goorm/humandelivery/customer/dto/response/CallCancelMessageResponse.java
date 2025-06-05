feaa4dcf13b7960613dcdad4e3a7bac4cd50fe5b
psg <1999byh@naver.com>
2025-06-05 10:05:16 +0900

    fix: WebSocketCancelCallControllerTest 테스트 깨짐 해결

--- src/main/java/goorm/humandelivery/customer/dto/response/CallCancelMessageResponse.java
+++ src/main/java/goorm/humandelivery/customer/dto/response/CallCancelMessageResponse.java
@@ -2,8 +2,12 @@ package goorm.humandelivery.customer.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CallCancelMessageResponse {
     private String message;
